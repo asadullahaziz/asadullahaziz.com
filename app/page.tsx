f506e3724531dced70f497c5745e82bb816701f5
Asad Ullah Aziz <asadullahaziz99@gmail.com>
2026-06-23 18:37:08 +0500

    Fix: hero page width

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
   return (
-    <PageWrapper>
+    <PageWrapper mainClassName="page-main--wide">
       <WebSiteSchema />
       <PersonSchema />
       <Hero />
